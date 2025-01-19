decc443d0bae4f9bc75a7efb4fa96f7262eea93b
Mohamad Marstias Billy <108399751+koncoweb@users.noreply.github.com>
2025-01-19 23:55:49 +0700

    ok
    new web online

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Albae Handicraft';
+export const SITE_DESCRIPTION = 'Kerajinan Tangan dan Kaligrafi Berkualitas';
